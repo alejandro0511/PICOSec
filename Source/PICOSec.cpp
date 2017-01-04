cf213172591b5bab1e060774b9c64e3a229f8406
tomatocolonel <tomatotaisa1943@gmail.com>
2017-01-04 23:07:34 +0900

    Determined directory structure, file structure, etc.
    +Added .gitignore
    +Added files and derectorys generated by MPLAB X IDE
    +Added ./Source
    +Added 2 source files; PICOSec.cpp, PICOSec.h

---
+++ Source/PICOSec.cpp
@@ -0,0 +1,16 @@
+/* ----- PICOSec.cpp ----- */
+/* ----- PICOSec's main program. Contains main() -----*/
+
+// ----- compiler: MPLAB XC32++ Compiler
+// ----- micro:    PIC32MX570F512L
+
+/*
+	This is the main program of PICOSec. It contains main().
+	Every sourcefile (*.cpp, *.c) and headerfile (*.h) should be at ./Source
+	MPLAB X IDE will devide each file into logical folders.
+*/
+
+int main(void){
+    
+    
+}
---
+++ Source/PICOSec.h
@@ -0,0 +1,19 @@
+/* ----- PICOSec.h ----- */
+/* ----- PICOSec's main header file. Includes all available headerfiles. -----*/
+
+// ----- compiler: MPLAB XC32++ Compiler
+// ----- micro:    PIC32MX570F512L
+
+/*
+    This file is expected to be included into every cpp file in PICOSec.
+    It includes all available headerfiles.
+    All cpp file has its own headerfiles, which declares functions available for other files,
+    as well as constants.
+    So, including this header file will also include every function and constant available in this project. 
+*/
+
+#ifndef PICOSEC_HEADER_INCLUDED
+#define PICOSEC_HEADER_INCLUDED
+
+
+#endif/*PICOSEC_HEADER_INCLUDED */
